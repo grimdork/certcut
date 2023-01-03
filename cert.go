c69fc967323f8a80c6ad367d49163147c280488c
Urethramancer <github@grimdork.net>
2023-01-03 14:39:47 +0100

    Merged cert and key files.

--- cert.go
+++ cert.go
@@ -65,6 +65,20 @@ func NewSerial() (*big.Int, error) {
 	return serial, nil
 }
 
+// NewKey creates a new RSA key for certificate generation and signing.
+func NewKey(bits int) (*rsa.PrivateKey, error) {
+	if bits < 2048 {
+		bits = 2048
+	}
+
+	key, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
 // NewCA creates a new certificate authority which further client certificates can be generated with.
 func NewCA(key *rsa.PrivateKey, cn string) ([]byte, error) {
 	serial, err := NewSerial()
